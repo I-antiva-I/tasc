6c4ff08613d9d302d636eb29f6b9a1a63af264e5
I-antiva-I <a.a.ivanichenko@gmail.com>
2024-04-26 21:30:52 +0200

    Controller class

--- main.py
+++ main.py
@@ -3,26 +3,18 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 
+from logic.controller import Controller
 from ui.widgets.windows.my_main_window import MyMainWindow
 
 
 def launch():
+    controller = Controller()
     app = QtWidgets.QApplication(sys.argv)
     app.setAttribute(Qt.AA_Use96Dpi)
-    main_window = MyMainWindow()
+    main_window = MyMainWindow(controller)
     main_window.show()
     sys.exit(app.exec_())
 
 
 if __name__ == '__main__':
     launch()
-
-
-'''
-I use PyQt extensively at work. All python code is using python naming
- conventions wherever possible, that is PascalCase for classes,
-  and under_score for variables and class members.
-   When internal PyQt methods/setters are called or overwritten,
-    then PyQt's conventions are used, naturally, but other than that
-     I try to stick to pythonic code as much as possible.
-'''
